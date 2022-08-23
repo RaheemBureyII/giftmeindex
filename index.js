c996f0628ed3df8ec4ff2f4330ec128c7bdec8f8
RaheemBureyII <94723743+RaheemBureyII@users.noreply.github.com>
2022-08-22 21:43:56 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+alert("i run");
+document.body.innerHTML="";
+var jsondata;
+window.addEventListener("flutterInAppWebViewPlatformReady",  function(event) {
+ const args = ["we in there like swimwear"];
+ window.flutter_inappwebview.callHandler('myHandlerName', ...args).then(function(result) {
+               jsondata=JSON.parse(result);
+<!--               alert(jsondata.company);-->
+             });;
+});
+
+window.$crisp=[];window.CRISP_WEBSITE_ID="78a9e559-e1a4-4b3a-9896-5c6c8ecf7caa";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+window.CRISP_RUNTIME_CONFIG = {
+	lock_maximized       : true,
+	lock_full_view       : true,
+	cross_origin_cookies : true
+};
+function onagenmessage(message){
+const args = [message.content];
+    window.flutter_inappwebview.callHandler('agentsentmessage',...args);
+}
+ window.CRISP_READY_TRIGGER =function(){
+    if($crisp.is("chat:opened")===true){
+    $crisp.push(["set", "user:email", [jsondata.email]]);
+    $crisp.push(["set", "user:nickname", [jsondata.name]]);
+    $crisp.push(["set", "user:company", [jsondata.company]]);
+    $crisp.push(["on", "message:received", onagenmessage])
+    }
+ }
