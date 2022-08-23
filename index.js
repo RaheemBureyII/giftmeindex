357c74e37f6b203bee5d0ad4e7c88d23270a70f8
RaheemBureyII <94723743+RaheemBureyII@users.noreply.github.com>
2022-08-22 21:51:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,106 @@
 alert("i run");
-document.body.innerHTML="";
+document.body.innerHTML="""<html>
+
+<head>
+    <title>Title of the document</title>
+    <style>
+        button.bg-transparent.spinner {
+    border-color: rgba(228, 228, 231, 1);
+}
+.spinner {
+    position: relative;
+    color: transparent !important;
+    pointer-events: none;
+    height: 100vh;
+  width: 100%;
+}
+
+.spinner:after {
+    content: '';
+    z-index: 3;
+    position: absolute !important;
+    top: calc(50% - (1em / 2));
+    left: calc(50% - (1em / 2));
+    display: block;
+    width: 7em;
+    height: 7em;
+    border: 20px solid #ff2c324a;
+    border-radius: 9999px;
+    border-right-color: transparent;
+    border-top-color: transparent;
+    animation: spinAround 500ms infinite linear;
+}
+
+@keyframes spinAround {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+html, body {
+  height: 100%;
+}
+body {
+  display: flex;
+}
+    </style>
+</head>
+
+<body>
+<div class="spinner" ></div>
+</body>
+<script type="text/javascript">
+
+</script>
+<script type="text/javascript">
+
+<!--var jsondata;-->
+<!--window.addEventListener("flutterInAppWebViewPlatformReady",  function(event) {-->
+<!-- const args = ["we in there like swimwear"];-->
+<!-- window.flutter_inappwebview.callHandler('myHandlerName', ...args).then(function(result) {-->
+<!--               jsondata=result;-->
+<!--             });;-->
+
+<!--});-->
+<!--var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date();-->
+<!--Tawk_API.onLoad = function(){-->
+<!--Tawk_API.maximize();-->
+
+<!--document.querySelector(".spinner").className="spin";-->
+<!--Tawk_API.setAttributes(JSON.parse(jsondata), function(error){});-->
+<!--};-->
+<!--Tawk_API.onChatMessageAgent = function(message){-->
+<!--const args = [message];-->
+<!--    window.flutter_inappwebview.callHandler('agentsentmessage',...args);-->
+<!--};-->
+<!--Tawk_API.onChatMinimized = function(){-->
+<!-- Tawk_API.maximize();-->
+<!-- const args = ["close"];-->
+<!--    window.flutter_inappwebview.callHandler('closemodal',...args);-->
+<!--};-->
+<!--Tawk_API.onChatEnded = function(){-->
+
+<!-- window.flutter_inappwebview.callHandler('endheadlesswebview', ...args);-->
+<!--    //place your code here-->
+<!--};-->
+
+<!--(function(){-->
+<!--var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];-->
+<!--s1.async=true;-->
+<!--s1.src='https://embed.tawk.to/622771cba34c2456412a0e97/1ftl190jj';-->
+<!--s1.charset='UTF-8';-->
+<!--s1.setAttribute('crossorigin','*');-->
+<!--s0.parentNode.insertBefore(s1,s0);-->
+<!--})();-->
+<!--function sendBack() {-->
+<!--                messageHandler.postMessage("Hello from JS");-->
+<!--             }-->
+</script>
+<!--End of Tawk.to Script-->
+
+</html>""";
 var jsondata;
 window.addEventListener("flutterInAppWebViewPlatformReady",  function(event) {
  const args = ["we in there like swimwear"];
